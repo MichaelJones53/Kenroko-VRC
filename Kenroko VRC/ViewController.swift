9e3c46fe9c2b1f77d5eb8944a05dbe77b283571a
Michael Jones <mykejonez53@gmail.com>
2017-12-05 14:44:56 -0800

    Initial Commit

---
+++ Kenroko VRC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kenroko VRC
+//
+//  Created by Michael Jones on 12/5/17.
+//  Copyright © 2017 Kenroko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
